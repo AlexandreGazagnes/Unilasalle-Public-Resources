812f6eb594b19ac95103350659264755c3420a31
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-03-18 11:56:39 +0100

    update

--- .utils/pre-push.sh
+++ .utils/pre-push.sh
@@ -3,15 +3,21 @@
 
 # black 
 black .
+black */*.ipynb
+black */*/*.ipynb
+black */*/*/*.ipynb
 
 # clean notebooks output
-# jupytext -clean --all *.ipynb
+jupyter nbconvert --clear-output --inplace */*.ipynb
+jupyter nbconvert --clear-output --inplace */*/*.ipynb
+jupyter nbconvert --clear-output --inplace */*/*/*.ipynb
 
 # build .html files 
-jupyter nbconvert --to html *.ipynb
 jupyter nbconvert --to html */*.ipynb
 jupyter nbconvert --to html */*/*.ipynb
 jupyter nbconvert --to html */*/*/*.ipynb
 
 # build .pdf files 
-# jupyter -nbconvert -to pdf *.ipynb
+jupyter nbconvert --to pdf */*.ipynb
+jupyter nbconvert --to pdf */*/*.ipynb
+jupyter nbconvert --to pdf */*/*/*.ipynb
