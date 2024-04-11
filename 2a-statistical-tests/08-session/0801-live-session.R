937a9a92ece75d6fa8bdd546379f2baa90ebaa4c
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-11 11:59:40 +0200

    udpate

--- 2a-statistical-tests/08-session/0801-live-session.R
+++ 2a-statistical-tests/08-session/0801-live-session.R
@@ -1,7 +1,7 @@
 # Questions 
 
 
-# 1st group 
+# 1st session (8h45 - 11.04.24) 
 
 ### 1st group 
 - Correlation between the "profile" of farmer (including gender) and exploitation type ? (production type, finacials results...)
@@ -9,9 +9,25 @@
 ### 2nd group 
 - **Charges**  production vs surface acording region ? 
 
-
 ## 3rd group
 - Bovins vs reste ==>
 
 ## # 4th group
-- Normandie vs reste du monde (UGB )?
+- Normandie vs reste du monde (UGB )? = > do we have 
+
+
+# 2st session (10h30 - 11.04.24)
+
+### 1st group 
+- is there a correlation between of expenses and in subssidies ?
+- cattle , H/F revenues specification 
+
+### 2nd group
+- Does sustainablacle  practices / areas impact economic viabililiy? 
+
+### 3rd group 
+- Is there a specific relationship between farmer'sprofile and ? (sustainability practice ==> not so much data ! )
+
+
+### 4th group
+- Is the **employement** rate france linked to evt constraint ?  ==> quantity of people working on the farm vs the size of the farm in evt constraints zone
