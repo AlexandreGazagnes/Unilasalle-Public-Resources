5f7cb586583d2c1ed7f3abdf509cb6de9aa52a40
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-11 10:37:25 +0200

    update

---
+++ 2a-statistical-tests/08-session/0801-live-session.R
@@ -0,0 +1,17 @@
+# Questions 
+
+
+# 1st group 
+
+### 1st group 
+- Correlation between the "profile" of farmer (including gender) and exploitation type ? (production type, finacials results...)
+
+### 2nd group 
+- **Charges**  production vs surface acording region ? 
+
+
+## 3rd group
+- Bovins vs reste ==>
+
+## # 4th group
+- Normandie vs reste du monde (UGB )?
