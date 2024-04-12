c77a9423c0228857ef65161a2174d87ec5682fe0
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-12 13:08:50 +0200

    udpate

--- 2a-statistical-tests/08-session/0801-live-session.R
+++ 2a-statistical-tests/08-session/0801-live-session.R
@@ -1,33 +1,58 @@
-# Questions 
+# Problems and Questions
 
 
-# 1st session (8h45 - 11.04.24) 
+## 1st session (8h45 - 2024-04-11) :
 
 ### 1st group 
-- Correlation between the "profile" of farmer (including gender) and exploitation type ? (production type, finacials results...)
+# - Correlation between the "profile" of farmer (including gender) and exploitation type ? (production type, finacials results...)
+# - Are the financial results of the farm linked to the profile of the farmer ?
+# - Are the young farmers more likely to have a specific type of production ?
+# - What about top 10 yound farmers  or old farmer?
+# - Do male/female managers have different production types ?
 
 ### 2nd group 
-- **Charges**  production vs surface acording region ? 
+# - **Charges**  (regading production type) vs surface according region ?
+# - Have certain specific regions or areas specific tax levels
+# - What is the link between type of production and costs?
+# - What is the impact of taxes and charges on the financial life of an agricultural operation?
 
-## 3rd group
-- Bovins vs reste ==>
-
-## # 4th group
-- Normandie vs reste du monde (UGB )? = > do we have 
-
-
-# 2st session (10h30 - 11.04.24)
-
-### 1st group 
-- is there a correlation between of expenses and in subssidies ?
-- cattle , H/F revenues specification 
-
-### 2nd group
-- Does sustainablacle  practices / areas impact economic viabililiy? 
-
-### 3rd group 
-- Is there a specific relationship between farmer'sprofile and ? (sustainability practice ==> not so much data ! )
-
+### 3rd group
+# - Cattle production type vs other production type (crops, etc) :  
+# - in terms of financial results
+# - in terms of size
+# - in terms of subsidies ....
+# - How Cattle production is significatively different from other production type ?
 
 ### 4th group
-- Is the **employement** rate france linked to evt constraint ?  ==> quantity of people working on the farm vs the size of the farm in evt constraints zone
+# - Normandy vs Rest of France? 
+# - Do we have a specific type of production in Normandy?
+# - Are your farm in Normandy more likely to have a specific financial result score ? 
+# - What patterns do we have regarding the famers'profiles ?
+
+
+## 2st session (10h30 - 11.04.24)
+#####################################################
+
+### 1st group 
+# - Is there a correlation between of expenses and subssidies ?
+# - This correlation is different according to the region or to the type of production ?
+# - Is there a correlation between the size of the farm and the subsidies ?
+# - The correlation is it related to the farmer profile ?
+# - Are the most profitable farms the ones that receive the most subsidies ?
+
+### 2nd group
+# - Does sustainablable practices / areas impact economic viabililiy?
+# - What are the main characterics of specific environmental areas => manager profile / financial results / production type
+# - Are the farms in these areas more profitable than the others ?
+# - Do theses areas have a specific financial trouble?
+
+### 3rd group 
+# - Is there a specific relationship between farmer'sprofile and sustainability practice?
+# - Do we have in the specific environmental areas a specific type of farmers ?
+# - Do we have younger farmers in these areas, or older farmers, more male than female ? 
+
+### 4th group
+# - Is the **employement** rate in France linked to environmental constraints ?  
+# - Is there any pattern regaring quantity of people working on the farm or the size of the farm in specific environmental protected areas?
+# - Are these farms going to hire permanent or fixed-term contracts anymore?
+# - Do these farms have a better yield per person?
