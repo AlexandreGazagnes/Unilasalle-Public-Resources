0cd9a83a5c96f97df45cf1c19826f9ce08215d48
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-12 16:30:38 +0200

    update

---
+++ 2a-statistical-tests/09-session/0901-Last-Session.R
@@ -0,0 +1,49 @@
+#################################
+#   0901 - Last Session         #
+#   TP09 - 1/1                  #
+#################################
+
+
+# Written by Alexandre Gazagnes
+# Last update: 2024-02-01
+
+
+## Summary
+#######################################
+
+
+# Powerpoint presentation  : 
+# From 10 to 20 slides
+# Structured approach
+# Plan :        1/ Context/Intro 
+#               2/ Data, preparation, cleaning etc 
+#               3/ Insights, analysis, visualisation, Tests
+#               Conclusion, recommendations, go further 
+
+
+# R script / notebook :
+# - Clean, structured, commented
+# - You can use Rmarkdown, Jupyter notebook, R script, etc
+# - 1 R script / notebooks is expected BUT feel free to add more if needed
+# - Feel free to use external libraiies, packages, etc
+# - Your code MUST BE reproducible ! 
+
+
+# Analysis :
+# - At least : quanti vs quanti / quanti vs quali / quali vs quali (quanti => numercial, quali => categorical)
+# - At least : 3 various types of analysis / questions / line of work for your  "big" problem
+
+
+# Visualisations :
+# - At least 5 different types of visualisation
+# - Box plots, pie charts, histograms, scatter plots, etc
+# - Feel free to use interactive visualisations, stacked bar charts, percentage bar charts, etc
+
+
+# Test hypothesis :
+# At least 5 different statistical tests such as : 
+#   1 correlation
+#   2 t test
+#   1 var test
+#   1 shapiro test
+#   1 chi2  (indepedance, homogeneity, etc)
