030428786ad183a35c3d76ea2a8cefa766c0851a
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-12 16:31:09 +0200

    update

--- 2a-statistical-tests/09-session/0901-Questions.R
+++ 2a-statistical-tests/09-session/0901-Questions.R
@@ -1,10 +1,9 @@
 # Problems and Questions
 
 
-## 1st session (8h45 - 2024-04-11)
-####################################################
 
-### 1st group 4th grou ==> Charline Lucille Edouard Antho"
+
+### "4th"  ==> Charline Lucille Edouard Antho
 # DEPRECATED : 
     # - Correlation between the "profile" of farmer (including gender) and exploitation type ? (production type, finacials results...)
     # - Are the financial results of the farm linked to the profile of the farmer ?
@@ -14,14 +13,14 @@
 
 
 
-### 2nd group ==> "5h group Orjete" ==>Oriane Tessa & Jeanne"
+### "5th Orjete" ==>O riane Tessa & Jeanne
 # - **Charges**  (regading production type) vs surface according region ?
 # - Have certain specific regions or areas specific tax levels
 # - What is the link between type of production and costs? => what is this difference about?
 # - What is the impact of taxes and charges on the financial state of a famr?
 
 
-### 3rd group "L314 => Leandre Julie Diego Eloise" s
+### "L314" => Leandre Julie Diego Eloise
 # - Cattle production type vs other production type (crops, etc) :  
 # - in terms of financial results
 # - in terms of size
@@ -29,24 +28,22 @@
 # - How Cattle production is significatively different from other production type ?
 
 
-### 4th group =>  "FEA 7 normands ==>Jeanne clement salame"
+### "FEA 7" aka "normandy forever" ==> Jeanne clement salame
 # - Normandy vs Rest of France? 
 # - Do we have a specific type of production in Normandy?
 # - Are your farm in Normandy more likely to have a specific financial result score ? 
 # - What patterns do we have regarding the famers'profiles ?
 
 
-## 2st session (10h30 - 11.04.24)
-####################################################
 
-### 1st group  "Coretin, Theresa, Kyle, Kate" => The best
+
+### "The Best"  ==> Coretin, Theresa, Kyle, Kate
 # DEPRECATED : 
     # - Is there a correlation between of expenses and subssidies ?
     # - This correlation is different according to the region or to the type of production ?
     # - Is there a correlation between the size of the farm and the subsidies ?
     # - The correlation is it related to the farmer profile ?
     # - Are the most profitable farms the ones that receive the most subsidies ?
-
 # NEW ONE
     # Farm types = production type => what age  sex and size of farm are the most profitable ? 
 
@@ -58,14 +55,13 @@
 # - Do theses areas have a specific financial trouble subsidies?
 
 
-### 3rd group   ==> "Future Farmers" ==> Charlotte Meissane & Ryan 
+### "Future Farmers" ==> Charlotte Meissane & Ryan 
 # - Is there a specific relationship between farmer'sprofile and environmental specific areas ?
 # - Do we have in the specific environmental areas a specific type of farmers ?
 # - Do we have younger farmers in these areas, or older farmers, more male than female ? 
 
 
-### 4th group => Queens " ==> Line Juliette Perrine & Nazar"
-
+### "Queens" ==> Line Juliette Perrine & Nazar
 # DEPRECATED BUT GOOD ;) 
     # - Is the **employement** rate in France linked to environmental constraints ?  
     # - Is there any pattern regaring quantity of people working on the farm or the size of the farm in specific environmental protected areas?
@@ -73,3 +69,4 @@
     # - Do these farms have a better yield per person?
 
 # NEW ONE : 
+    # ?
